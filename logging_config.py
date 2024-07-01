856041c477e1e1a0ba3d5192ab45243601c9dbbd
sanch13 <sanch13sanch@gmail.com>
2024-07-01 11:50:55 +0300

    delete show log console

--- logging_config.py
+++ logging_config.py
@@ -7,6 +7,6 @@ import os
 if not os.path.exists('logs'):
     os.makedirs('logs')
 
+logger.remove()
 
 logger.add("logs/logs.log", rotation="10 MB", retention="10 days", compression="zip")
-logger.add(sys.stderr, level="WARNING")
